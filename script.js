83b2e9117cf799c5dcd18042ea5eef9c50b7b152
Davyde35 <david.dumastpl@gmail.com>
2023-07-17 12:36:57 +0200

    ajout d'un son au click

--- script.js
+++ script.js
@@ -6,37 +6,40 @@ let cost = 2;
 function actualisation_html() {
     document.querySelector('#resultat').innerText = watts;
     document.querySelector('#cost_upgrade').innerText = cost + ' watts';
-   
 }
-function clicker () {
+
+const powerClicked = () => {
+
     watts += nb_watts_clck;
     console.log("watts", watts);
     actualisation_html();
-}
 
-document.querySelector('#power_img').addEventListener("click", clicker);
+}
+//document.querySelector('#power_img').addEventListener("click", powerClicked); 
+
 document.querySelector(".upgrade").addEventListener("click", function(){
     if(watts >= cost) {
         watts = watts - cost;
-        actualisation_html()
         nb_watts_clck++;
-        cost = cost*2;
-    } else {
-        actualisation_html("")
-    }
+        cost = cost + 2;
+    } 
+    actualisation_html()
 });
 
 
 function createParticule(x, y) {
     const powerClicks = document.querySelector(".power-clicks");
-
     const particule = document.createElement("img");
-    particule.setAttribute("src", "./img/logo_energie2.png");
+    particule.setAttribute("src", "./img/power.png");
     particule.setAttribute("class", "power-particule");
     particule.style.left = x + "px";
     particule.style.top = y + "px";
 
     powerClicks.appendChild(particule);
+    
+       const clickSound = document.getElementById("son");
+       clickSound.currentTime = 0;
+       clickSound.play();
 
     setTimeout(() => {
         powerClicks.removeChild(particule);
@@ -44,8 +47,8 @@ function createParticule(x, y) {
 }
 
 const power_img = document.querySelector("#power_img");
-power_img.addEventListener("click", (e) => {
-    createParticule(e.clientX, e.clientY);
+power_img.addEventListener("click", (evenement) => {
+    createParticule(evenement.clientX, evenement.clientY);
     powerClicked();
 });
 
